765cff5b3861cd1f70563f0012db724e6130755d
f@gmail.com <ludaosina79@gmail.com>
2023-12-22 16:19:01 +0300

    final version

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Database settings 
+
+DATABASE_PATH = '../college_app/src/server/database'
+DATABASE_NAME = 'college.db'
+
+# Server settings
+
+HOST = '127.0.0.1'
+PORT = 8000
+URL = f'http://{HOST}:{PORT}'
---
+++ src/server/database/pydantic_models.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ModifyBaseModel(BaseModel):
+    id: Optional[int] = 0
+
+
+class ChangePassword(ModifyBaseModel):
+    password: str
+
+
+class LoginData(ModifyBaseModel):
+    login: str
+    password: str
+
+
+class User(ModifyBaseModel):
+    position: str
+    login: str
+    password: str
+    power_level: int 
+
+
+class Student(ModifyBaseModel):
+    student_id: int
+    first_name: str
+    last_name: str
+    grade: str
+
+
+class Teacher(ModifyBaseModel):
+    teacher_id: int
+    first_name: str
+    last_name: str
+    subject: str
+
+
+class Class(ModifyBaseModel):
+    class_name: str
+    teacher_id: int
+
+
+class Subject(ModifyBaseModel):
+    subject_name: str
+
+
+class Grade(ModifyBaseModel):
+    student_id: int
+    subject_id: int
+    grade_value: float
